3e572f607a964e53edc4ee74a746004ea13d3c22
ibrahim -raad <ibrahim00raad@gmail.com>
2025-11-01 10:55:34 +0200

    removed length limitation

--- src/domain/countries/entities/country.entity.ts
+++ src/domain/countries/entities/country.entity.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { AbstractEntity } from 'src/domain/shared/abstract.entity';
 import { Column, Entity, Index } from 'typeorm';
 
@@ -7,7 +7,6 @@ import { Column, Entity, Index } from 'typeorm';
 export class Country extends AbstractEntity {
   @IsNotEmpty()
   @IsString()
-  @MinLength(3)
   @Column({ nullable: false, length: 255, unique: true })
   name: string;
 }
